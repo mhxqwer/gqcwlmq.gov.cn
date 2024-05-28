5e296928a8d158a8a99d6f09e332b8237b524d83
mhxqwer <168000686+mhxqwer@users.noreply.github.com>
2024-05-28 20:45:02 +0800

    Add files via upload

---
+++ static/js/gov_dates.js
@@ -0,0 +1,8 @@
+﻿var weeks = ["日","一","二","三","四","五","六"],
+    today = new Date(),
+    year = today.getFullYear(),
+    month = today.getMonth()+1,
+    week = today.getDay(),
+    date = today.getDate();
+    time = year + "年" + month + "月" + date +"日" +" 星期"+weeks[week];
+document.write(time);
